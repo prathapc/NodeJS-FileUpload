1b970446cff39891e7c7bf0573d1846f820e21f7
prathapc <prathap.choudhury@gmail.com>
2014-11-12 22:48:13 +0530

    exec cmd executed in req handle

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = []
-handle["/"] = requestHandlers.start;
+var handle = {}
+handle["/"]  = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(route, handle){
-function onRequest(request,response){
-	console.log("request received");
-	
+function start(route, handle) {
+    function onRequest(request,response){
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for "+pathname+" received");
 	
-	route(handle, pathname);
-
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello World!");
-	response.end();
-
-}
+	route(handle, pathname, response);
+   }
 
 http.createServer(onRequest).listen(8888);
-console.log("server has started");
+console.log("Server has started..");
 }
-
 exports.start = start;
